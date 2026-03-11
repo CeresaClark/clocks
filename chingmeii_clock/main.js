1a6a1829bc70332a4f6d98f1ea37cd89e1dfc520
CeresaClark <157949231+CeresaClark@users.noreply.github.com>
2026-03-12 02:08:09 +0800

    v1.0

---
+++ chingmeii_clock/main.js
@@ -0,0 +1,31 @@
+
+function startTime() {
+    var today = new Date();
+    var yy = today.getFullYear(),
+        mon = today.getMonth() + 1,
+        dd = today.getDate(),
+        hh = today.getHours(),
+        mm = today.getMinutes();
+
+    dd = String(dd).padStart(2, '0')
+    hh = String(hh).padStart(2, '0')
+    mm = String(mm).padStart(2, '0')
+    mon = String(mon).padStart(2, '0')
+
+
+
+    $('.day,.dayB').html(yy + '.' + mon + '.' + dd);
+    $('.hour').html(hh);
+    $('.min').html(mm);
+    var timeoutId = setTimeout(startTime, 1000);
+}
+
+function blink() {
+    var $b = $('.blink');
+    setInterval(function () {
+        $b.css('opacity', $b.css('opacity') == '0' ? '1' : '0');
+    }, 1000);
+}
+
+startTime()
+blink()
